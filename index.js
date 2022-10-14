d65bff87e3e9374abb15de59bc19907e63e0f47b
SeymaDemir <seymademir35@gmail.com>
2022-10-14 15:54:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, eventNAme){
+    let newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        
+        newArr.push(`Thank you ${names[i]}, for the wonderful ${eventNAme} gift!`)
+        
+    }
+    console.log(newArr);
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num){
+    while(0 <= num){
+        console.log(num--);
+    }
+}
+
+countDown(10);
